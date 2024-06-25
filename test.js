42f1412b33df35bb6ea043739d6e91a802554467
Adam Whitlock <adam.whitlock@adhocteam.us>
2024-06-25 13:16:18 -0600

    testing

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 // CodeQL wont pass if it doesn't find any code in the repo that it can scan
 // this file is just to see if this will trigger a scan to successfully run
 
-const foo = bar;
+const foo = 'bar';
 console.log('test.js loaded', foo);
 
